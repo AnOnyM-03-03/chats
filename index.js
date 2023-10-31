003d0ff8c1dcc74f41d2ed7c96aadcef62cf3890
Tolstov <tolstov.dmitrij42@gmail.com>
2023-10-31 14:32:19 +0400

    project skeleton and server connection

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/',(req,res) =>{
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+app.listen(5000, ()=> {
+  console.log('started');
+})
